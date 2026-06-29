baef5558f371e727d956ac734880ea0434eae0c9
Alex Cruz <alexcruz@MacBook-Pro-de-Alex.local>
2026-06-29 16:41:10 -0300

    fix: downgrade worklets and exclude from autolinking

---
+++ react-native.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  dependencies: {
+    'react-native-worklets': {
+      platforms: {
+        android: null, // disables autolinking on android
+        ios: null,     // disables autolinking on ios
+      },
+    },
+  },
+};
